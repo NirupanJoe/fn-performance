ebc3b0d3aba372f01a0eea1474510a7f0c15a473
njj <njj@laufire.in>
2022-10-26 10:30:34 +0530

    Introduced a loop find.

--- bennyExample.js
+++ bennyExample.js
@@ -1,9 +1,22 @@
 const { suite, add, cycle, complete, save } = require('benny');
-const { map, filter, find, reduce } = require('@laufire/utils/collection')
+const { map, filter, find, reduce, range } = require('@laufire/utils/collection');
+const { rndBetween } = require('@laufire/utils/random');
 
-const numbers = [1, 2, 3, 4];
+const numbers = range(0, 100).map((value, i, array) =>
+rndBetween(1, array.length));
 
-const odd = (value) => value % 2; 
+const odd = (value) => value % 2;
+
+const loopFind = (data) => {
+		let index = 0;
+
+		while (index < data.length && !odd(data[index], index, data)) {
+
+			index++;
+		}
+
+		return data[index];
+	};
 
 const suites = [
 	{
@@ -11,11 +24,11 @@ const suites = [
 	  tests: [
 			{
 				name: 'js-utils map',
-				test: () => map(numbers, (num) => num + 1)
+				test: () => map(numbers, odd)
 			},
 			{
 				name: 'native map',
-				test: () => numbers.map((num) => num + 1),
+				test: () => numbers.map(odd),
 			},
 		]
 	},
@@ -42,6 +55,10 @@ const suites = [
 			{
 				name: 'native find',
 				test: () => numbers.find(odd),
+			},
+			{
+				name: 'loop find',
+				test: () => loopFind(numbers),
 			},
 		]
 	},
