60f60a1507c3fa6628b90a5357ea7e283fd85b3f
njj <njj@laufire.in>
2022-11-14 10:57:18 +0530

    Refactor a testRandom.

--- testRandom.js
+++ testRandom.js
@@ -2,14 +2,14 @@ const { range, keys } = require('@laufire/utils/collection');
 const { rndBetween, rndValues } = require('@laufire/utils/random');
 const { suite, add, cycle, complete } = require('benny');
 
-const max = 100;
+const itemCount = 100;
 
-const array = range(0, max).map((value, i, array) =>
+const array = range(0, itemCount).map((value, i, array) =>
 	rndBetween(1, array.length));
 
 const obj = Object.fromEntries(array.map((val, key) => [key, val]));
 
-const arrayRandomKeys = rndValues(keys(array));
+const arrayRandomKeys = rndValues(keys(array), itemCount / 2);
 const objRandomKeys = arrayRandomKeys.map((key) => String(key));
 
 const suites = [
